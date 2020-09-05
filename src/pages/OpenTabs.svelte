efa630726e27b26ac68aefb0c7f7cb9b46da6119
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-09-05 16:45:39 +0530

    save open links to collection

--- src/pages/OpenTabs.svelte
+++ src/pages/OpenTabs.svelte
@@ -1,22 +1,38 @@
 <script>
     import { onMount } from 'svelte';
     import { getCurrentWindowTabs } from '../services/chrome';
-    import { initOpenTabsState } from '../sync';
+    import { initOpenTabsState, openTabsStateToStorage } from '../sync';
+    import { uniqueId } from '../utils';
 
     import OpenTabsView from './OpenTabsView.svelte';
 
     let links = [];
+    let collections = [];
+    let collectionName = '';
 
     onMount(async () => {
         const tabs = await getCurrentWindowTabs();
-        const { openLinks, collections } = await initOpenTabsState(tabs);
+        const { openLinks, collections: cols } = await initOpenTabsState(tabs);
 
         links = openLinks;
-
-        console.log(tabs);
-        console.log(openLinks);
-        console.log(collections);
+        collections = cols;
     });
+
+    async function saveCollection(event) {
+        const { collectionName } = event.detail;
+
+        const newCollection = {
+            id: uniqueId(),
+            name: collectionName,
+            createAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+        };
+        collections = [...collections, newCollection];
+        await openTabsStateToStorage(links, collections, newCollection.id);
+    }
 </script>
 
-<OpenTabsView {links} />
+<OpenTabsView
+    {links}
+    {collectionName}
+    on:saveClick={saveCollection} />
