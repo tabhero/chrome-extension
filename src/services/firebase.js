1a5035c7818b29763e1891e3730533bb92fa6e9e
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-10-02 12:59:15 +0530

    plug firebase into svelte store

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -16,3 +16,4 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
+export const firestore = firebase.firestore();
--- src/store.js
+++ src/store.js
@@ -1,4 +1,27 @@
-import { writable } from 'svelte/store';
+import { writable, readable } from 'svelte/store';
+
+import { firestore } from './services/firebase';
 
 export const currentTabTags = writable([]);
 export const currentTabLink = writable(null);
+
+export const tags = readable([], function start(set) {
+    const unsubscribe = firestore.collection('tags').onSnapshot({
+        next: (snapshot) => {
+            const tags = snapshot.docs.map(documentSnapshot => documentSnapshot.data());
+            set(tags);
+        },
+        error: (error) => {
+            console.log(error);
+        }
+    });
+    return function stop() {
+        unsubscribe();
+    };
+});
+
+export const addTag = (tag) => {
+    const { id, name } = tag;
+    return firestore.collection('tags').doc(id).set({ name })
+        .catch(err => console.error(err));
+};
