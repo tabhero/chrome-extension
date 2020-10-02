6706024bf4e77b44aeb3a33b38f7a45cc92eff0a
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-10-02 17:16:05 +0530

    listen to tabUpdate and linkUpdate events

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import { writable, readable } from 'svelte/store';
 
 import { firestore } from './services/firebase';
+import { getCurrentTab, registerOnTabUpdate } from './services/chrome';
+import { normalUrl, linkFromTab } from './utils';
 
 export const currentTabTags = writable([]);
 export const currentTabLink = writable(null);
@@ -17,6 +19,51 @@ export const tags = readable([], function start(set) {
     });
     return function stop() {
         unsubscribe();
+    };
+});
+
+const listenLinkUpdate = (url, callback) => {
+    return firestore.collection('links')
+        .where('url', '==', normalUrl(url))
+        .onSnapshot(snapshot => {
+            if (snapshot.empty) {
+                return callback(null);
+            }
+            const doc = snapshot.docs[0];
+            const link = {
+                ...doc.data(),
+                id: doc.id,
+            };
+            callback(link);
+        });
+};
+
+export const currentLink = readable(null, async (set) => {
+    let unsubLinkUpdate = () => {};
+
+    const currentTab = await getCurrentTab();
+    unsubLinkUpdate = listenLinkUpdate(currentTab.url, (link) => {
+        if (link) {
+            set(link);
+        } else {
+            set(linkFromTab(currentTab));
+        }
+    });
+
+    const unsubscribeTabUpdate = registerOnTabUpdate(newTab => {
+        unsubLinkUpdate();
+        unsubLinkUpdate = listenLinkUpdate(newTab.url, (link) => {
+            if (link) {
+                set(link);
+            } else {
+                set(linkFromTab(newTab));
+            }
+        });
+    });
+
+    return () => {
+        unsubLinkUpdate();
+        unsubscribeTabUpdate();
     };
 });
 
